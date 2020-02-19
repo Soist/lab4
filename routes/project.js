2a02c6a07795f78d64f9f8f8a308e180a7be0339
Li Manqi <limanqi.lmq@gmail.com>
2020-02-19 13:59:39 +0800

    correct

--- routes/project.js
+++ routes/project.js
@@ -1,7 +1,3 @@
-exports.view = function(req,res){
-	res.render('index',{
-		'name' : 'Wait in Line',
-		'image' : 'lorempixel.people.1.jpeg',
-		'id' : 'project1'
-	});
+exports.viewProject = function(req,res){
+	res.render("project");
 };
